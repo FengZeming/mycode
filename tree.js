e90e7d2e9475f10daf100e19f68e0dee4b1bc881
FengZeming <23966213@qq.com>
2016-03-02 22:44:31 +0800

    测试

--- tree.js
+++ tree.js
@@ -1,3 +1,9 @@
+/*
+   未完成 遍历目录使用同步方式
+ * 
+ * */
+
+
 var nPath = require('path');
 var nFs = require('fs');
 
@@ -5,33 +11,38 @@ var arry = [];
 var fuhao = '* ';
 var suojin = ' ';
 var index = 0;
+
 fileEach();
 
-console.log(arry);
-
-function fileEach(path) {
-	var path = path || __dirname;
-	index++;
-	nFs.readdir(path, function(err, files) {
-		if (err) throw err;
+function fileEach() {
+	if (nFs.existsSync('SUMMARY.md')) {
+		nFs.unlink('SUMMARY.md', function(e) {
+			if (e) throw e;
+			create();
+		});
+	} else {
+		create();
+	}
+};
+function create(path) {
+	var path = path || (arry = [], index = 0, __dirname);
+	nFs.readdir(path, function(e, files) {
+		if (e) throw e;
 		files.forEach(function(filename) {
-			nFs.stat(nPath.join(path, filename), function(err, stats) {
-				if (err) throw err;
+			nFs.stat(nPath.join(path, filename), function(e, stats) {
+				if (e) throw e;
 				if (filename.charAt(0) === '.') return;
 				if (stats.isFile()) {}
 				if (stats.isDirectory()) {
-					console.log()
+					arry.push(filename);
+					index = path.substring(__dirname.length).split(nPath.sep).length;
 					writeFile(getSuojin(index) + fuhao + filename);
-					fileEach(nPath.join(path, filename));
+					create(nPath.join(path, filename));
 				}
 			});
 		});
 	});
-};
-
-
-
-
+}
 function getSuojin(len) {
 	var suojin = '';
 	for (var i = 0; i < len; i++) {
@@ -39,15 +50,12 @@ function getSuojin(len) {
 	}
 	return suojin;
 }
-
-
 function writeFile(data) {
-	nFs.open('SUMMARY.md','a', 0644, function(e, fd) {
+	nFs.open('SUMMARY.md', 'a', 0644, function(e, fd) {
 		if (e) throw e;
 		nFs.write(fd, data + '\n', 0, 'utf8', function(e) {
 			if (e) throw e;
 			nFs.closeSync(fd);
 		})
 	});
-
 }
---
+++ treeSync.js
@@ -0,0 +1,75 @@
+var nPath = require('path');
+var nFs = require('fs');
+var arry = [];
+var index = 0;
+var mdName = 'SUMMARY.md';
+var aExclude = ['jq.js', 'README.md', 'tree.js', 'treeSync.js'];
+var rPath = '';
+
+
+//调用方法
+main();
+
+
+//主方法
+function main() {
+	nFs.existsSync(mdName) && nFs.unlinkSync(mdName);
+	createData();
+}
+//创建数据
+function createData(path) {
+	var path = path || (arry = [], __dirname);
+	var files = nFs.readdirSync(path); //同步读取文件
+	//遍历
+	files.forEach(function(filename) {
+		if (filename.charAt(0) === '.') return;
+		//同步读取文件信息
+		var stat = nFs.statSync(nPath.join(path, filename));
+		//文件层级 
+		index = path.substring(__dirname.length).split(nPath.sep).length;
+		if (index === 1) {
+			arry = [];
+		}
+		//如果文件是文件夹
+		if (stat.isDirectory()) {
+			if (!arry[index - 1]) {
+				arry.push(filename);
+			} else {
+				arry[index - 1] = filename;
+			}
+			//写入文件
+			writeData(filename, index, 'd');
+			//递归
+			createData(nPath.join(path, filename));
+		}
+		if (stat.isFile() && aExclude.indexOf(filename) === -1) {
+			writeData(filename, index);
+		}
+	});
+}
+//层级字符串
+function getLevel(len) {
+	var level = '';
+	for (var i = 0; i < len; i++) {
+		level += ' ';
+	}
+	return level;
+}
+//获取组织好的数据
+function getData(filename, index, type) {
+	var type = type || 'f';
+	return getLevel(index) + '* ' + '[' + filename + '](.' + nPath.sep + arry.join(nPath.sep) + (type === 'd' ? '' : nPath.sep + filename) + ')\n';
+}
+
+
+//写入数据
+function writeData(filename, index, type) {
+	var data = getData(filename, index, type);
+	nFs.open(mdName, 'a', 0644, function(e, fd) {
+		if (e) throw e;
+		nFs.write(fd, data, 0, 'utf8', function(e) {
+			if (e) throw e;
+			nFs.closeSync(fd);
+		})
+	});
+}
