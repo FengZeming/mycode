e11c71c56ef5ee32fdaae11d27aa65d109bab9cd
FengZeming <23966213@qq.com>
2017-08-01 10:55:43 +0800

    添加 问卷调查demo

--- works/question/static/js/main.js
+++ works/question/static/js/main.js
@@ -9,7 +9,7 @@
     var baseUrl = Nenu.CONSTANT.PATH.STATIC;
 
     require.config = {
-        baseUrl : baseUrl ,
+        baseUrl : 'static/' ,
         map     : {
             '*' : {
                 'css' : requirePath + '/css' ,
